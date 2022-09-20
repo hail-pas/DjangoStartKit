99c82add02a36ce14f5433bed93f249aa6103968
panhq <hypofiasco@gmail.com>
2022-09-20 18:03:33 +0800

    optimize authorized permission

--- apis/permissions.py
+++ apis/permissions.py
@@ -16,35 +16,51 @@ class URIBasedPermission(BasePermission):
         return bool(request.user and request.user.is_authenticated) and request.user.has_api_perm(request, view)
 
 
-class AuthorizedServicePermission(BasePermission):
+class AuthorizedServicePermission(
+    BasePermission
+):
     def has_permission(self, request, view):
-        identifier = request.headers.get("x-identifier")
+        request.caller = None
+        identifier = request.headers.get('x-identifier')
         timestamp = request.headers.get("x-timestamp")
         sign_str = request.headers.get("x-sign")
         if not all([identifier, timestamp, sign_str]):
+            logger.warning("参数不全")
             return False
-        # caller = ThirdService.objects.filter(identifier=identifier).first()
-        # 替换成对应的授权调用方实例
+        # caller =
         caller = None
         if not caller:
+            logger.warning("Identifier未找到")
             return False
-        if request.method in ["GET", "DELETE"]:
-            sign_data = request.GET
+        if request.method.upper() in ['GET', 'DELETE']:
+            sign_data = request.GET.dict()
         else:
-            sign_data = request.data
+            sign_data = request.data.dict()
         logger.debug(f"sign_data: {sign_data}")
         if not timestamp:
+            logger.warning("缺少时间戳")
             return False
-        if not isinstance(timestamp, int):
+        try:
+            timestamp = int(timestamp)
+        except ValueError:
+            logger.warning("时间戳格式错误")
             return False
         if not sign_str:
+            logger.warning("签名丢失")
             return False
         if int(time.time()) - timestamp > 120 or int(time.time()) - timestamp < -120:
+            logger.warning("请求过期")
             return False
         # sign_data 生成签名
-        sign_data_str = join_params(sign_data)
-        sign_data_str = f"{sign_data_str}&api_ky={caller.api_key}&timestamp={timestamp}"
+        sign_data_str = "".join(join_params(sign_data, initial=True, filter_none=True))
+        sign_data_str = f"{sign_data_str}&api_key={caller.api_key}&timestamp={timestamp}"
         logger.debug(f"sign_data_str: {sign_data_str}")
+        # print(sign_data)
+        # print(sign_data_str)
+        # print(caller.sign_key)
+        # print(caller.api_key)
         if not SignAuth(caller.sign_key).verify(sign_str, sign_data_str):
+            logger.warning("签名校验失败")
             return False
+        request.caller = caller
         return True
